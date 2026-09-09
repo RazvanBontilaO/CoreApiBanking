f45d43c7df6dfaa595acaba8fda4947bfcda343e
RazvanBontilaO <razvan.bontila@student.upt.ro>
2026-09-09 16:26:09 +0300

    Introduce AuditService for logging transactions with a new transactional context

---
+++ src/main/java/com/example/banking/service/AuditService.java
@@ -0,0 +1,25 @@
+package com.example.banking.service;
+
+import com.example.banking.model.Status;
+import com.example.banking.model.Transaction;
+import com.example.banking.repository.TransactionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+
+@Service
+@RequiredArgsConstructor
+public class AuditService {
+
+    private final TransactionRepository transactionRepository;
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void logTransaction(String fromAccountNumber, String toAccountNumber, BigDecimal amount) {
+        Transaction transaction = new Transaction(fromAccountNumber, toAccountNumber,
+                amount, Status.SUCCESS);
+        transactionRepository.save(transaction);
+    }
+}
