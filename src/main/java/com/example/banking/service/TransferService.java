ba7d992ff896c2f481f79b13a4ee8fc846044bce
RazvanBontilaO <razvan.bontila@student.upt.ro>
2026-09-09 16:13:20 +0300

    Log transactions in TransferService with success and failure status

--- src/main/java/com/example/banking/service/TransferService.java
+++ src/main/java/com/example/banking/service/TransferService.java
@@ -3,7 +3,10 @@ package com.example.banking.service;
 import com.example.banking.exception.AccountNotFoundException;
 import com.example.banking.exception.InsufficientFundsException;
 import com.example.banking.model.Account;
+import com.example.banking.model.Status;
+import com.example.banking.model.Transaction;
 import com.example.banking.repository.AccountRepository;
+import com.example.banking.repository.TransactionRepository;
 import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,7 +17,9 @@ import java.math.BigDecimal;
 @RequiredArgsConstructor
 public class TransferService {
 
+
     private final AccountRepository accountRepository;
+    private final TransactionRepository transactionRepository;
 
     @Transactional
     public void transferMoney(String fromAccountNumber, String toAccountNumber, BigDecimal amount) {
@@ -24,10 +29,12 @@ public class TransferService {
                 .orElseThrow(() -> new AccountNotFoundException("Destination account not found"));
 
         if (fromAccount.getBalance().compareTo(amount) < 0) {
+            transactionRepository.save(new Transaction(fromAccountNumber, toAccountNumber, amount, Status.FAILED));
             throw new InsufficientFundsException("Insufficient fonds in account");
         }
 
         fromAccount.setBalance(fromAccount.getBalance().subtract(amount));
         toAccount.setBalance(toAccount.getBalance().add(amount));
+        transactionRepository.save(new Transaction(fromAccountNumber, toAccountNumber, amount, Status.SUCCESS));
     }
 }
